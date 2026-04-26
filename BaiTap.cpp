5da6e7dc65f4101e7ec1890a586fd23c0a1641a4
ngothimaianh <anh.ntm2413974@sis.hust.edu.vn>
2026-04-26 14:46:29 +0700

    pj2

--- BaiTap.cpp
+++ BaiTap.cpp
@@ -33,6 +33,10 @@ Node* TaoNodeMoi(SinhVien x){
     p ->link= NULL;
     return p;
 }
+void XoaBoNhoDem() {
+    char c;
+    while (cin.get(c) && c != '\n');
+}
 // so sánh chuỗi
 int SoSanhChuoi(char s1[], char s2[]) {
     int i = 0;
@@ -43,6 +47,17 @@ int SoSanhChuoi(char s1[], char s2[]) {
     }
     if (s1[i] == '\0' && s2[i] == '\0') return 0;
     return (s1[i] == '\0') ? -1 : 1;
+}
+void NhapChuoi(char s[], int DoDaiMax) {
+    char c;
+    int i = 0;
+    while (i < DoDaiMax - 1) {
+        cin.get(c); 
+        if (c == '\n') break; 
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
 }
 // Chen vao danh sach co thu tu
 void ChenDanhSach(List &l, SinhVien x){
@@ -64,4 +79,40 @@ if (p-> link == NULL) {
     l.cuoi = p;
 }
        }
+ void NhapMotSV(SinhVien &sv) {
+    cout << "Nhap Ma SV: "; 
+    cin >> sv.maSV;
+    XoaBoNhoDem(); 
+
+    cout << "Nhap Ho Ten: "; 
+    NhapChuoi(sv.HoTen, 50);
+
+    cout << "Nhap Gioi Tinh (0:Nam, 1:Nu): "; 
+    cin >> sv.GioiTinh;
+
+    cout << "Nhap Ngay, Thang, Nam sinh: ";
+    cin >> sv.NgaySinh.ngay >> sv.NgaySinh.thang >> sv.NgaySinh.nam;
+    XoaBoNhoDem();
+
+    cout << "Nhap Dia chi: "; 
+    NhapChuoi(sv.DiaChi, 100);
+
+    cout << "Nhap Lop: "; 
+    cin >> sv.Lop;
+
+    cout << "Nhap Khoa: "; 
+    cin >> sv.Khoa;
+}
+
+void XuatDanhSach(List l) {
+    cout << "\n--- DANH SACH SINH VIEN (TANG DAN THEO MA SV) ---\n";
+    Node *p = l.dau;
+    while (p != NULL) {
+        cout << p->data.maSV << " | " << p->data.HoTen << " | " 
+             << p->data.NgaySinh.ngay << "/" << p->data.NgaySinh.thang << "/" << p->data.NgaySinh.nam 
+             << " | Lop: " << p->data.Lop << endl;
+        p = p->link;
+    }
+}
+
     
