91661c2389975718a7dd3fc762a606f80256c45e
ngothimaianh <anh.ntm2413974@sis.hust.edu.vn>
2026-04-26 15:43:45 +0700

    pj3

--- BaiTap.cpp
+++ BaiTap.cpp
@@ -28,7 +28,7 @@ void DanhSachRong(List &l){
 }
 Node* TaoNodeMoi(SinhVien x){
     Node *p = new Node;
-    if (p = NULL) return NULL;
+    if (p == NULL) return NULL;
     p ->data = x;
     p ->link= NULL;
     return p;
@@ -79,29 +79,22 @@ if (p-> link == NULL) {
     l.cuoi = p;
 }
        }
- void NhapMotSV(SinhVien &sv) {
-    cout << "Nhap Ma SV: "; 
-    cin >> sv.maSV;
-    XoaBoNhoDem(); 
-
-    cout << "Nhap Ho Ten: "; 
-    NhapChuoi(sv.HoTen, 50);
-
-    cout << "Nhap Gioi Tinh (0:Nam, 1:Nu): "; 
-    cin >> sv.GioiTinh;
-
-    cout << "Nhap Ngay, Thang, Nam sinh: ";
-    cin >> sv.NgaySinh.ngay >> sv.NgaySinh.thang >> sv.NgaySinh.nam;
-    XoaBoNhoDem();
-
-    cout << "Nhap Dia chi: "; 
-    NhapChuoi(sv.DiaChi, 100);
-
-    cout << "Nhap Lop: "; 
-    cin >> sv.Lop;
-
-    cout << "Nhap Khoa: "; 
-    cin >> sv.Khoa;
+void SaoChepChuoi(char dich[], char nguon[]) {
+    int i = 0;
+    while (nguon[i] != '\0') {
+        dich[i] = nguon[i];
+        i++;
+    }
+    dich[i] = '\0'; // Kết thúc chuỗi bằng ký tự rỗng
+}
+void GanSV(SinhVien &sv, char ma[], char ten[], int gt, int d, int m, int y, char lp[]) {
+    SaoChepChuoi(sv.maSV, ma);
+    SaoChepChuoi(sv.HoTen, ten);
+    sv.GioiTinh = gt;
+    sv.NgaySinh.ngay = d;
+    sv.NgaySinh.thang = m;
+    sv.NgaySinh.nam = y;
+    SaoChepChuoi(sv.Lop, lp);
 }
 
 void XuatDanhSach(List l) {
@@ -115,4 +108,87 @@ void XuatDanhSach(List l) {
     }
 }
 
+// 3.In sinh viên cùng ngày sinh
+bool TrungNgaySinh(Ngay n1, Ngay n2) {
+    return (n1.ngay == n2.ngay && n1.thang == n2.thang && n1.nam == n2.nam);
+}
+void SinhVienCungNgaySinh(List l) {
+    bool TimThay = false;
+    cout << "\nCAC SINH VIEN CO CUNG NGAY SINH\n";
+
+    for (Node *p = l.dau; p != NULL; p = p->link) {
+        bool trung = false;
+        for (Node *q = l.dau; q != NULL; q = q->link) {
+            if (p != q && TrungNgaySinh(p->data.NgaySinh, q->data.NgaySinh)) {
+                trung = true;
+                break;
+            }
+        }
+        
+
+        if (trung) {
+            cout << p->data.maSV << " | " << p->data.HoTen << " | " 
+                 << p->data.NgaySinh.ngay << "/" << p->data.NgaySinh.thang << "/" << p->data.NgaySinh.nam << endl;
+            TimThay = true;
+        }
+    }
     
+
+    if (!TimThay) {
+        cout << "Khong tim thay sinh vien cung ngay sinh" << endl;
+    }
+}
+//4. Loại bỏ sinh viên có cùng ngày sinh
+bool KiemTraCoTrung(List l, Node* k) {
+    for (Node *p = l.dau; p != NULL; p = p->link) {
+        if (p != k && TrungNgaySinh(p->data.NgaySinh, k->data.NgaySinh))
+            return true;
+    }
+    return false;
+}
+
+void XoaSVTrungNgaySinh(List &l) {
+    Node *ht = l.dau;
+    Node *truoc = NULL;
+
+    while (ht != NULL) {
+        if (KiemTraCoTrung(l, ht)) {
+            Node *temp = ht;
+            if (truoc == NULL) { 
+                l.dau = ht->link;
+                ht = l.dau;
+            } else {
+                truoc->link = ht->link;
+                ht = truoc->link;
+            }
+            
+            if (ht == NULL) l.cuoi = truoc;
+            
+            delete temp; // Giải phóng bộ nhớ
+        } else {
+            truoc = ht;
+            ht = ht->link;
+        }
+    }
+    cout << "\nDa loai bo cac sinh vien co cung ngay sinh!" << endl;
+}
+int main() {
+    List l; DanhSachRong(l);
+    SinhVien s1, s2, s3;
+    
+    // Bây giờ việc tạo dữ liệu chỉ mất 1 dòng mỗi người
+    GanSV(s1, "20241111", "Nguyen Van Vinh", 0, 19, 7, 2006, "ET1");
+    ChenDanhSach(l, s1);
+
+    GanSV(s2, "20241112", "Tran Thi Binh", 1, 19, 7, 2006, "ET1");
+    ChenDanhSach(l, s2);
+
+    GanSV(s3, "20241113", "Le Van Chau", 0, 20, 5, 2005, "ET1");
+    ChenDanhSach(l, s3);
+
+    XuatDanhSach(l);
+    SinhVienCungNgaySinh(l);
+    XoaSVTrungNgaySinh(l);
+    XuatDanhSach(l);
+    return 0;
+}
